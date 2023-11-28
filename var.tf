2b545ee001e9c9e14483cf21bd0cc91b0ef0b4df
gith <149611366+git01h@users.noreply.github.com>
2023-11-28 21:36:35 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,1 +1,10 @@
-
+variable "rg_name" {
+  type        = any
+  default     = "Harsh_terraform"
+  description = "The name of the resource group in which to create the subnet. Changing this forces a new resource to be created."
+}
+variable "rg_location" {
+  type        = string
+  # default     = "North Europe"
+  description = "The location/region where the virtual network is created."
+}
