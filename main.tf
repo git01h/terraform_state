2332afe7eb2709556fd21cce5546bace80ed906c
gith <149611366+git01h@users.noreply.github.com>
2023-11-29 15:13:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,10 @@ resource "azurerm_resource_group" "example" {
   name     = var.rg_name
   location = var.rg_location
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = var.sa_name
+  resource_group_name      = azurerm_resource_group.az.name
+  location                 = azurerm_resource_group.az.location
+  account_tier             = var.sa_account_tier
+  account_replication_type = var.account_replication_type
 
