db544321e7c41dd49e0df6404d668dcd721df6db
gith <149611366+git01h@users.noreply.github.com>
2023-11-30 15:12:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ backend "azurerm" {
     resource_group_name  = "example"
     storage_account_name = "sa07896"
     container_name       = "tfstate"
-    key                  = "root.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
